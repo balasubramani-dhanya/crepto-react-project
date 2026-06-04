ed5e9c53abe1627d78b9481fd883512ccaedc6e0
balasubramani-dhanya <143580206+balasubramani-dhanya@users.noreply.github.com>
2026-06-04 20:00:59 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import '../App.css'
 import Navbar from '../components/Navbar'
-import Hero from '../components/hero'
+import Hero from '../components/Hero'
 import Lottery from '../components/Lottery'
 import Stats from '../components/Stats'
 import Features from '../components/Feature'
 import Cards from '../components/Card'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import Coins from '../components/Coins'
 
 
